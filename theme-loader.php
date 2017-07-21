d677dd96ea60d781b43a90604a03e10dc7340938
Truong Giang <truongwp@gmail.com>
2017-07-21 21:45:39 +0700

    Add theme loader for using in theme

--- theme-loader.php
+++ theme-loader.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Gallery meta box
+ * Gallery meta box loader for using in theme
  *
  * @package Gallery_Meta_Box
  * @author Truong Giang <truongwp@gmail.com>
@@ -11,8 +11,8 @@ if ( defined( 'TRUONGWP_GALLERY_META_BOX_PATH' ) ) {
 	return;
 }
 
-define( 'TRUONGWP_GALLERY_META_BOX_PATH', plugin_dir_path( __FILE__ ) );
-define( 'TRUONGWP_GALLERY_META_BOX_URL', plugin_dir_url( __FILE__ ) );
+define( 'TRUONGWP_GALLERY_META_BOX_PATH', get_template_directory() . '/gallery-meta-box/' );
+define( 'TRUONGWP_GALLERY_META_BOX_URL', get_template_directory_uri() . '/gallery-meta-box/' );
 
 require_once TRUONGWP_GALLERY_META_BOX_PATH . 'class-truongwp-gallery-meta-box.php';
 
@@ -23,4 +23,4 @@ function truongwp_gallery_meta_box_init() {
 	$meta_box = new Truongwp_Gallery_Meta_Box();
 	$meta_box->init();
 }
-add_action( 'plugins_loaded', 'truongwp_gallery_meta_box_init' );
+add_action( 'after_setup_theme', 'truongwp_gallery_meta_box_init' );
